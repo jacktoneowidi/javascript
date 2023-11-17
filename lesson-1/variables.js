c03037ec8634e9d54d19a38753f9ac99e2694015
jacktone owidi <kingsowidi@gmail.com>
2023-11-17 10:25:17 +0300

    am learning about varaibles

---
+++ lesson-1/variables.js
@@ -0,0 +1,25 @@
+var petdog ='rex'
+var petcat ='pepper'
+
+console.log(
+    'my pet dog name is ' + petdog
+)
+console.log(
+    'my cat pet name is ' + petcat
+
+)
+var dogsound ='woof'
+var catsound ='purr'
+
+console.log(
+    petdog+ ' says ' + dogsound
+
+)
+console.log(
+    petcat + ' says ' + catsound
+)
+var catsound = 'meow'
+
+console.log(
+    petcat + ' now says ' + catsound 
+)
